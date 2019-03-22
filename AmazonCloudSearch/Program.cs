70e172f1475457e7709e88c96cb0110fcb78cf82
Ralph Capper <ralph_22@hotmail.com>
2019-03-22 14:23:38 +0000

    Add project files.

---
+++ AmazonCloudSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AmazonCloudSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
